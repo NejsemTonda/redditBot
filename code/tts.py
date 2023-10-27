e8765e0cd030520ac3993e6e5cd1058d92d55233
nejsemTonda <vaclav@krnak.cz>
2023-10-27 21:00:36 +0200

    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

--- code/tts.py
+++ code/tts.py
@@ -1,30 +1,68 @@
 import os
-import azure.cognitiveservices.speech as speechsdk
+import elevenlabs
+import random 
 
+elevenlabs.set_api_key(os.environ["ELEVENLABS_TOKEN"])
+
+voice_ids = [
+    "pNInz6obpgDQGcFmaJgB",
+    "ErXwobaYiN019PkySvjV",
+    "VR6AewLTigWG4xSOukaG",
+    "EXAVITQu4vr4xnSDxMaL",
+    "N2lVS1w4EtoT3dr4eOWO",
+    "IKne3meq5aSn9XLyUdCD",
+    "XB0fDUnXU5powFXDhCwa",
+    "2EiwWnXFnvU5JabPnv8n",
+    "onwK4e9ZLuTAKqWW03F9",
+    "CYw3kZ02Hs0563khs1Fj",
+    "AZnzlk1XvdvUeBnXmlld",
+    "ThT5KcBeYPX3keUQqHPh",
+    "MF3mGyEYCl7XYWbV9V6O",
+    "LcfcDJNUP1GQjkzn1xUU",
+    "g5CIjZEefAph4nQFvHAz",
+    "D38z5RcWu1voky8WS1ja",
+    "jsCqWAovK2LkecY7zXl4",
+    "jBpfuIE2acCO8z3wKNLl",
+    "zcAOhNBS3c14rBihAFp1",
+    "z9fAnlkpzviPz146aGWa",
+    "oWAxZDx7w5VEj9dCyTzz",
+    "SOYHLrjzK2X1ezoPC6cr",
+    "ZQe5CZNOzWyzPSCn5a3c",
+    "bVMeCyTHy58xNoL34h3p",
+    "t0jbNlBVZ17f02VDIeMI",
+    "Zlb1dXrM653N07WRdFW3",
+    "TxGEqnHWrfWFTfGW9XjX",
+    "TX3LPaxmHKxFdv7VOQHJ",
+    "XrExE9yKIg1WjnnlVkGX",
+    "Yko7PKHZNXotIFUBG7I9",
+    "flq6f7yk4E4fJM5XTYuZ",
+    "zrHiDhphv9ZnVXBqCLjz",
+    "piTKgcLEGmPE4e6mEKli",
+    "ODq5zmih8GrVes37Dizd",
+    "21m00Tcm4TlvDq8ikWAM",
+    "wViXBPUzp2ZZixB1xQuM",
+    "yoZ06aMxZJJ28mfd3POQ",
+    "pMsXgVXv3BLzUgSXRplE",
+    "GBv7mTt0atIp3Br8iCZE"
+]
 
 
 def synt_voice(text, name):
-    # This example requires environment variables named "SPEECH_KEY" and "SPEECH_REGION"
-    speech_config = speechsdk.SpeechConfig(subscription=os.environ.get('SPEECH_KEY'), region=os.environ.get('SPEECH_REGION'))
-    audio_config = speechsdk.audio.AudioOutputConfig(use_default_speaker=True)
+    voice = elevenlabs.Voice(
+        voice_id = random.choice(voice_ids),
+        settings = elevenlabs.VoiceSettings(
+            stability = 0, # 0 stability sound best to me. Other settins shows no effect on the voice
+            similarity_boost = 0.75,
+            style = 0,
+            use_speaker_boost = True
+        )
+    )
+    audio = elevenlabs.generate( 
+        text = text,
+        voice = voice
+    )
     
-    # The language of the voice that speaks.
-    speech_config.speech_synthesis_voice_name='en-US-JasonNeural'
-    
-    speech_synthesizer = speechsdk.SpeechSynthesizer(speech_config=speech_config, audio_config=audio_config)
-    
-    # Get text from the console and synthesize to the default speaker.
-    
-    speech_synthesis_result = speech_synthesizer.speak_text_async(text).get()
-    
-    if speech_synthesis_result.reason == speechsdk.ResultReason.SynthesizingAudioCompleted:
-        print("Speech synthesized for text [{}]".format(text))
-    
-    elif speech_synthesis_result.reason == speechsdk.ResultReason.Canceled:
-        cancellation_details = speech_synthesis_result.cancellation_details
-        print("Speech synthesis canceled: {}".format(cancellation_details.reason))
-        if cancellation_details.reason == speechsdk.CancellationReason.Error:
-            if cancellation_details.error_details:
-                print("Error details: {}".format(cancellation_details.error_details))
-                print("Did you set the speech resource key and region values?")
-    
+    elevenlabs.save(audio, f"../src/voices/{name}.mp3")
+
+
+synt_voice("hello, this is fucking awesome. Why didn't I find it sooner!", "0001")
