f2b0120b6c0f655d21d6798a999aae0ec0bbd209
nejsemTonda <vaclav@krnak.cz>
2023-10-27 21:55:30 +0200

    very small progress on scraping

--- code/scraping.py
+++ code/scraping.py
@@ -1,8 +1,21 @@
 #import praw
 import requests
-import selenium
-
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time
+import os 
 '''
 Scraping is going to be hard propably sinvce reddit's changes to is API. Might use manual scraping instead
 '''
+
+
+driver = webdriver.Firefox()
+driver.get("https://www.reddit.com/")
+
+username = "HappyLittleRedditBot"
+password = os.environ["reddit_password"]
+
+login_button = driver.find_element("link text", "Log In")
+login_button.click()
+
 
