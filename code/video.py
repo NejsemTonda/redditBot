3a9e46acf29a0c5f821ecc1c4ecdee819a83fb53
nejsemTonda <vaclav@krnak.cz>
2023-10-28 00:59:25 +0200

    some work on video

--- code/video.py
+++ code/video.py
@@ -1,5 +1,16 @@
-import moviepy.editor as ed
+import moviepy.editor as mpy
+from moviepy.video.fx.all import crop
 import random
+import os
+
+def crop(video):
+    new_height = video.size[1]
+    new_width = int(new_height * 9/16)
+    
+    x_center = (video.size[0]) // 2
+    y_center = (video.size[1] - new_height) // 2
+    return  crop(video, width=new_width, height=new_height, x_center=x_center, y_center=y_center)
+    
 
 def get_random_interval(clip, t=60):
     duration = clip.duration
@@ -11,10 +22,39 @@ def get_random_interval(clip, t=60):
 
     return clip.subclip(start, end)
 
+def word_animation(word, start_time, ats):
+    txt_clip = mpy.TextClip(word, font="Amiri-Bold", fontsize=100, color='white')
+    txt_clip = txt_clip.set_duration(ats)
+    txt_clip = txt_clip.set_start(start_time)
+    return txt_clip
+
+
+def get_text(name):
+    with open(f"../src/text/{name}", 'r') as file:
+        return list(map(str.strip, file.readlines()))
 
 
 
+background = mpy.VideoFileClip("../src/background/cropped_trackmania.webm")
+audios = [mpy.AudioFileClip("../src/voices/"+name) for name in os.listdir("../src/voices") if "001-" in name]
+videos = []
+time_offset = 0
+for i, post in enumerate(get_text("001.txt")):
+    words = post.split()
+    audio = audios[i]
+    ats = audio.duration / len(words)
+    
+    clips = [word_animation(word, time_offset + j*ats, ats) for j, word in enumerate(words)]
+    video = mpy.CompositeVideoClip([c.set_pos('center') for c in clips], size=(1920,1020))
 
-clip = ed.VideoFileClip("../src/background/trackmaina.webm")
-#clip.write_videofile("001.mp4")
+    audio = audio.set_start(time_offset)
+    video = video.set_audio(audio)
+    time_offset += audio.duration + 0.5
+    
+    videos.append(video)
 
+videos = list(map(crop, videos))
+background = get_random_interval(background, time_offset)
+video = mpy.CompositeVideoClip(videos+[background])
+
+video.write_videofile("word_animation.mp4", codec='libx264', fps=12)
