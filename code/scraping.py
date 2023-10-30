f583698bf46f6861f5eb0437a2eec0b611ea56f5
nejsemTonda <vaclav@krnak.cz>
2023-10-30 15:28:41 +0100

    scraping of reddit page

--- code/scraping.py
+++ code/scraping.py
@@ -1,21 +1,31 @@
-#import praw
 import requests
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import time
 import os 
+from bs4 import BeautifulSoup
+import json 
+
 '''
 Scraping is going to be hard propably sinvce reddit's changes to is API. Might use manual scraping instead
 '''
 
+def get_html(urls):    
+    driver = webdriver.Firefox()
+    driver.get("https://www.reddit.com/login")
+    
+    username = "HappyLittleRedditBot"
+    password = os.environ["reddit_password"]
+    
+    driver.find_element("id", "loginUsername").send_keys(username)
+    driver.find_element("id", "loginPassword").send_keys(password)
+    driver.find_element("xpath", "/html/body/div/main/div[1]/div/div[2]/form/fieldset[5]/button").click()
+    time.sleep(8)
 
-driver = webdriver.Firefox()
-driver.get("https://www.reddit.com/")
+    response = []
+    for target in urls:
+        driver.get(target)
+        response.append(driver.page_source)
 
-username = "HappyLittleRedditBot"
-password = os.environ["reddit_password"]
-
-login_button = driver.find_element("link text", "Log In")
-login_button.click()
-
+    return response
 
