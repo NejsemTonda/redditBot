d9fcf9f3316d30f1f959790743c482aa8c64b283
nejsemTonda <vaclav@krnak.cz>
2023-11-02 23:07:04 +0100

    filtered out female voices

--- code/tts.py
+++ code/tts.py
@@ -8,24 +8,14 @@ voice_ids = [
     "pNInz6obpgDQGcFmaJgB",
     "ErXwobaYiN019PkySvjV",
     "VR6AewLTigWG4xSOukaG",
-    "EXAVITQu4vr4xnSDxMaL",
     "N2lVS1w4EtoT3dr4eOWO",
     "IKne3meq5aSn9XLyUdCD",
-    "XB0fDUnXU5powFXDhCwa",
     "2EiwWnXFnvU5JabPnv8n",
     "onwK4e9ZLuTAKqWW03F9",
     "CYw3kZ02Hs0563khs1Fj",
-    "AZnzlk1XvdvUeBnXmlld",
-    "ThT5KcBeYPX3keUQqHPh",
-    "MF3mGyEYCl7XYWbV9V6O",
-    "LcfcDJNUP1GQjkzn1xUU",
     "g5CIjZEefAph4nQFvHAz",
     "D38z5RcWu1voky8WS1ja",
-    "jsCqWAovK2LkecY7zXl4",
-    "jBpfuIE2acCO8z3wKNLl",
     "zcAOhNBS3c14rBihAFp1",
-    "z9fAnlkpzviPz146aGWa",
-    "oWAxZDx7w5VEj9dCyTzz",
     "SOYHLrjzK2X1ezoPC6cr",
     "ZQe5CZNOzWyzPSCn5a3c",
     "bVMeCyTHy58xNoL34h3p",
@@ -33,20 +23,14 @@ voice_ids = [
     "Zlb1dXrM653N07WRdFW3",
     "TxGEqnHWrfWFTfGW9XjX",
     "TX3LPaxmHKxFdv7VOQHJ",
-    "XrExE9yKIg1WjnnlVkGX",
     "Yko7PKHZNXotIFUBG7I9",
     "flq6f7yk4E4fJM5XTYuZ",
-    "zrHiDhphv9ZnVXBqCLjz",
-    "piTKgcLEGmPE4e6mEKli",
     "ODq5zmih8GrVes37Dizd",
-    "21m00Tcm4TlvDq8ikWAM",
     "wViXBPUzp2ZZixB1xQuM",
     "yoZ06aMxZJJ28mfd3POQ",
-    "pMsXgVXv3BLzUgSXRplE",
-    "GBv7mTt0atIp3Br8iCZE"
+    "GBv7mTt0atIp3Br8iCZE",
 ]
-
-
+    
 def synt_voice(text, name):
     voice = elevenlabs.Voice(
         voice_id = random.choice(voice_ids),
@@ -63,6 +47,3 @@ def synt_voice(text, name):
     )
     
     elevenlabs.save(audio, f"../src/voices/{name}.mp3")
-
-
-synt_voice("hello, this is fucking awesome. Why didn't I find it sooner!", "0001")
