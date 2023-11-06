dfb2b216d28057d756fc897efde7671a2963fc26
nejsemTonda <vaclav@krnak.cz>
2023-11-07 00:05:50 +0100

    tied everything together

--- code/main.py
+++ code/main.py
@@ -7,22 +7,24 @@ from video import create_video
 
 
 def main(args):
+    print(args)
     if args.filter:
         with open("scraped.txt", 'r') as file:
             scraped = file.read().split()
     else:
         scraped = []
-    links = get_reddits() # unfiltred links
-    links = list(filter(lambda x: x not in scraped, links))   
+    #links = get_reddits() # unfiltred links
+    #links = list(filter(lambda x: x not in scraped, links))   
 
-    #links = ["https://www.reddit.com/r/AskReddit/s/q4tcjAv0hA", "https://www.reddit.com/r/AskReddit/s/JYvp76RmyM"]
+    links = ["https://www.reddit.com/r/AskReddit/s/q4tcjAv0hA", "https://www.reddit.com/r/AskReddit/s/JYvp76RmyM"]
 
     
     qnas = [get_QnA(link) for link in links]
+    qnas = [x[:5] for x in qnas]
     
     while len(args.names) != 0 and len(args.names) != len(qnas):
         print("there is not enough names for the links, would you like to spice more names for the videos? (-a if auto)")
-        name = input("name for {qnas[len(args.names)][0]}: ")
+        name = input(f"name for {qnas[len(args.names)][0]}: ")
         if name == "-a":
             name = f"{len(os.listdir('../videos')):04}"
 
@@ -34,16 +36,18 @@ def main(args):
             args.names.append(f"{c:04}")
             c += 1
     
-    for name, qna in zip(name, qnas):
-        for i, text in enumerate(qnas):
+
+    for name, qna in zip(args.names, qnas):
+        for i, text in enumerate(qna):
+            print(f"synthesize {text} as {name}-{i}")
             synt_voice(text, name+f"-{i}")
 
-        create_video(name)
+        create_video(name, qna)
     
 if __name__ == "__main__":
     parser = ArgumentParser()
     parser.add_argument("-f", "--filter", action="store_false", help="filter out all the already done reddits")
-    parser.add_argument("-n", "--name", nargs="*", help="name of the video (number if not specified)")
+    parser.add_argument("-n", "--names", nargs="*", default=[], help="name of the video (number if not specified)")
     args = parser.parse_args()
 
     main(args)
