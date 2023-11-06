2bfffbb0e640b34081bacd425023102867c7e96d
nejsemTonda <vaclav@krnak.cz>
2023-11-06 23:29:02 +0100

    fixed scraping

--- code/scraping.py
+++ code/scraping.py
@@ -5,27 +5,23 @@ Scraping is going to be hard propably sinvce reddit's changes to is API. Might u
 '''
 
 def get_json(url):
-    # this is not working, reddit usses diferent id for sharing and for threads 
-    raise NotImplementedError("")
-    re
-    tokens = url.split("/")
-    reddit_id = tokens[-1]
+    req = requests.get(url)
+    real_url = req.history[0].text
+    tokens = real_url.split("/")
     reddit = tokens[tokens.index('r')+1]
+    reddit_id = tokens[tokens.index('comments')+1]
+    
 
-    #request.history
     return requests.get(f"https://www.reddit.com/r/{reddit}/comments/{reddit_id}.json", headers = {'User-agent': 'your bot 0.1'}).json()
 
 def get_QnA(url):
     data = get_json(url)
-    print(data)
     
     question = data[0]['data']['children'][0]['data']['title']
     
     responses = []
     for c in data[1]['data']['children']:
-        response.append(c['data']['body'])
+        if 'body' in c['data']:
+            responses.append(c['data']['body'])
 
     return [question]+responses
-
-
-print(get_QnA("https://www.reddit.com/r/AskReddit/s/q4tcjAv0hA"))
