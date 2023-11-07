629bf3d4eb592ad82a2cdeafeeb691591a4cecdd
nejsemTonda <vaclav@krnak.cz>
2023-11-07 22:38:15 +0100

    only generate 60s of AI voice content

--- code/main.py
+++ code/main.py
@@ -4,7 +4,12 @@ from tts import synt_voice
 from fbScraping import get_reddits
 from scraping import get_QnA
 from video import create_video
+from mutagen.mp3 import MP3
 
+def get_voice_len(name):
+    audio = MP3(f"../src/voices/{name}.mp3")
+    return audio.info.length
+    
 
 def main(args):
     print(args)
@@ -13,14 +18,14 @@ def main(args):
             scraped = file.read().split()
     else:
         scraped = []
+    print("getting reddits")
     #links = get_reddits() # unfiltred links
     #links = list(filter(lambda x: x not in scraped, links))   
 
     links = ["https://www.reddit.com/r/AskReddit/s/q4tcjAv0hA", "https://www.reddit.com/r/AskReddit/s/JYvp76RmyM"]
 
-    
+    print("getting qnas")
     qnas = [get_QnA(link) for link in links]
-    qnas = [x[:5] for x in qnas]
     
     while len(args.names) != 0 and len(args.names) != len(qnas):
         print("there is not enough names for the links, would you like to spice more names for the videos? (-a if auto)")
@@ -36,13 +41,19 @@ def main(args):
             args.names.append(f"{c:04}")
             c += 1
     
-
+    
     for name, qna in zip(args.names, qnas):
+        audio_len = 0
         for i, text in enumerate(qna):
             print(f"synthesize {text} as {name}-{i}")
             synt_voice(text, name+f"-{i}")
-
+            audio_len += get_voice_len(name+f"-{i}") + 5
+            if audio_len > 60:
+                qna = qna[:i]
+                break
+            
         create_video(name, qna)
+        quit()
     
 if __name__ == "__main__":
     parser = ArgumentParser()
